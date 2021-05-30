9f218de3bb03e54c1b03988a1afb9eee270167ef
aiwytmm <annbazhenova16@gmail.com>
2021-05-31 00:09:48 +0500

    refactored cut filters creation

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -242,10 +242,7 @@ void updateCoefficients(Coefficients &old, const Coefficients &replacements) {
 }
 
 void EQAudioProcessor::updateLowCutFilters(const ChainSettings& chainSettings) {
-    auto lowCutCoefficients = juce::dsp::FilterDesign<float>::designIIRHighpassHighOrderButterworthMethod(
-        chainSettings.lowCutFreq,
-        getSampleRate(),
-        2 * (chainSettings.lowCutSlope + 1));
+    auto lowCutCoefficients = makeLowCutFilter(chainSettings, getSampleRate());
 
     auto& leftLowCut = leftChain.get<ChainPositions::LowCut>();
     auto& rightLowCut = rightChain.get<ChainPositions::LowCut>();
@@ -255,10 +252,7 @@ void EQAudioProcessor::updateLowCutFilters(const ChainSettings& chainSettings) {
 }
 
 void EQAudioProcessor::updateHighCutFilters(const ChainSettings& chainSettings) {
-    auto highCutCoefficients = juce::dsp::FilterDesign<float>::designIIRLowpassHighOrderButterworthMethod(
-        chainSettings.highCutFreq,
-        getSampleRate(),
-        2 * (chainSettings.highCutSlope + 1));
+    auto highCutCoefficients = makeHighCutFilter(chainSettings, getSampleRate());
 
     auto& leftHighCut = leftChain.get<ChainPositions::HighCut>();
     auto& rightHighCut = rightChain.get<ChainPositions::HighCut>();
